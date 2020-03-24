a693496dee7d4f0741b95b29fbaf4e1ebcc7ceb6
github TakumiFuzawa <futaku0811@gmail.com>
2020-03-24 15:24:48 +0900

    create delete

--- firebase_test/MyPage/Setting/MyPageSettingViewController.swift
+++ firebase_test/MyPage/Setting/MyPageSettingViewController.swift
@@ -12,16 +12,53 @@ import PGFramework
 // MARK: - Property
 class MyPageSettingViewController: BaseViewController {
     
+    var userModel: UserModel = UserModel()
+    
+    @IBOutlet var mailTextField: UITextField!
+    @IBOutlet var passTextField: UITextField!
+    @IBAction func touchedDoneButton(_ sender: UIButton) {
+        UserModel.update(request: userModel) {
+            self.dismiss(animated: true, completion: nil)
+        }
+    }
+    @IBAction func touchedCancelButton(_ sender: UIButton) {
+        
+        dismiss(animated: true, completion: nil)
+    }
+    @IBAction func touchedLogoutButton(_ sender: UIButton) {
+        UserModel.logOut {
+            self.dismiss(animated: true, completion: nil)
+        }
+    }
+    @IBAction func toucheddeleteButton(_ sender: UIButton) {
+        UserModel.delete {
+            self.dismiss(animated: true, completion: nil)
+        }
+    }
 }
 
 // MARK: - Life cycle
 extension MyPageSettingViewController {
-    
+    override func loadView() {
+        super.loadView()
+        mailTextField.text = userModel.mail
+        passTextField.text = userModel.password
+        mailTextField.delegate = self
+        passTextField.delegate = self
+        //        UserModel.update(request: userModel) {
+        //            self.userModel.mail = self.mailTextField.text
+        //            self.userModel.password = self.passTextField.text
+        //        }
+    }
 }
 
 // MARK: - Protocol
-extension MyPageSettingViewController {
-    
+extension MyPageSettingViewController: UITextFieldDelegate{
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        userModel.mail = mailTextField.text
+        userModel.password = passTextField.text
+        return true
+    }
 }
 
 // MARK: - method
