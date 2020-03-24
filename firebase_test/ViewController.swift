104d31bec96108f6096221691cd82421dc6300b1
github TakumiFuzawa <futaku0811@gmail.com>
2020-03-24 11:56:14 +0900

    Initial Commit

---
+++ firebase_test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  firebase_test
+//
+//  Created by Takumi Fuzawa on 2020/03/24.
+//  Copyright © 2020 Takumi Fuzawa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ firebase_testTests/firebase_testTests.swift
@@ -0,0 +1,34 @@
+//
+//  firebase_testTests.swift
+//  firebase_testTests
+//
+//  Created by Takumi Fuzawa on 2020/03/24.
+//  Copyright © 2020 Takumi Fuzawa. All rights reserved.
+//
+
+import XCTest
+@testable import firebase_test
+
+class firebase_testTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
